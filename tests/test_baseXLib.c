af6053869cabeebcdfdd2807ea0029e31e94e490
Adrian STEINER <steia19@bfh.ch>
2025-03-30 13:27:25 +0200

    Add test files

---
+++ tests/test_baseXLib.c
@@ -0,0 +1,31 @@
+/**
+ * @file test_baseXLib.c
+ * @author Adrian STEINER (steia19@bfh.ch)
+ * @brief test application for the baseX library
+ * @version 0.1
+ * @date 30-03-2025
+ *
+ * @copyright (C) 2025 Adrian STEINER
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
+#include "unity.h"
+#include <string.h>
+
+int main(void) {
+  UNITY_BEGIN();
+  // RUN_TEST(test_isLeapYear);
+  return UNITY_END();
+}
